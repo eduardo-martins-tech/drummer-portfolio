38fefaec4b6d8f7e3b1fb69c36f5d19c41a18669
Eduardo Martins <eduardomartinsdf290@gmail.com>
2026-07-24 10:42:37 -0300

    Initialize React project and create navigation bar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,32 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Eduardo Martins</h1>
+
+      <ul>
+        <li>
+            Início
+        </li>
+        <li>
+            História
+        </li>
+        <li>
+            Vídeos
+        </li>
+        <li>
+            Galeria
+        </li>
+        <li>
+            Performances
+        </li>
+        <li>
+            Contato
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
